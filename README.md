cef42bd7f79097f22b85b86adf34dd6b877cb819
ben1sheff <ben1sheff@berkeley.edu>
2024-07-05 22:50:38 -0600

    Set it to auto-detect the dog and separate that part of the image into a 512x512

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dog_recognition_flywheel
 The goal of this project is to make a script to recognize when my dog is sitting and throw a ball when he does
+
+Note: current object recognition uses TensorFlow lite, and requires tflite_support>=0.2, which is unavailable for Python >=3.10 as of 7/5/2024. 
--- data_gathering.py
+++ data_gathering.py
@@ -16,7 +16,7 @@ webcam = "/dev/video0"
 # Threading for TF
 num_threads = 4
 # Display parameters
-dispW = 720  # 1280  # 640 # 
+dispW = 1280  # 1280  # 640 # 
 dispH = 720  # 720  # 480  #
 # File to save pictures to
 pictures_file = "pictures.hdf5"
@@ -28,9 +28,12 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 font_size = 0.7
 font_weight = 2
 font_color = (0, 155, 0)
+# Do we do print statements?
+verbose = True
+printv = lambda *args, **kwargs: print(*args, **kwargs) if verbose else None
 # Setting up object detection
 base_options = core.BaseOptions(file_name=models, use_coral=False, num_threads=num_threads)
-detection_options = processor.DetectionOptions(max_results=2, score_threshold = 0.3) # how many objects, how sure to be
+detection_options = processor.DetectionOptions(max_results=4, score_threshold = 0.3) # how many objects, how sure to be
 options = vision.ObjectDetectorOptions(base_options=base_options, detection_options=detection_options)
 detector = vision.ObjectDetector.create_from_options(options)
 
@@ -54,11 +57,17 @@ def add_picture():
     global take_pic_flag
     take_pic_flag = True
 button.when_pressed = add_picture
-pictures = None
+pictures = []
 # Find the actual resolution, since it forcably changes it
 ret, image = cam.read()
 dispH = len(image)
 dispW = len(image[0])
+
+# Detecting if Boson goes off camera
+def edge_detection(x, y, w, h):
+    return (min(max(x, 0), max(dispW - w - 1, 0)),
+            min(max(y, 0), max(dispH - h - 1, 0)),
+            w, h)
 
 # Loop setup stuff
 take_pic_flag = False
@@ -75,24 +84,54 @@ while not exit_flag:
     ret, image = cam.read()
     # image = picam.capture_array()
     # image = cv2.flip(image, 1)
-    # Cut us down to 512x512
-    image = image[dispH//2 - pic_dim//2: dispH//2 + pic_dim//2,
-                  dispW//2 - pic_dim//2: dispW//2 + pic_dim//2]
+    # # Cut us down to 512x512
+    # image = image[dispH//2 - pic_dim//2: dispH//2 + pic_dim//2,
+    #               dispW//2 - pic_dim//2: dispW//2 + pic_dim//2]
 
-    # Making an array of pictures
+
+    # Tensorflow
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    im_tensor = vision.TensorImage.create_from_array(image_rgb)
+    det_objs = detector.detect(im_tensor)
+    # Do something with the data
+    # Find the dog and photograph him
     if take_pic_flag:
-        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        timer = time.time()
-        take_pic_flag = False
-        cv2.imshow("picture", image_gray)
-        cv2.waitKey(2000)
-        if take_pic_flag:
-            if pictures is None:
-                pictures = np.array([image_gray], dtype=np.uint8)
+        dog_bounds = []
+        for det_obj in det_objs.detections:
+            label = det_obj.categories[0].category_name
+            if label == "dog" or label == "cat":
+                square_side = max(det_obj.bounding_box.width,  det_obj.bounding_box.height)
+                dog_bounds = list(edge_detection(det_obj.bounding_box.origin_x,  det_obj.bounding_box.origin_y,
+                                                 square_side, square_side))
+                break
+        if len(dog_bounds):
+            printv(dog_bounds)
+            dog_area = cv2.resize(image[dog_bounds[1]:dog_bounds[1]+dog_bounds[3],
+                                        dog_bounds[0]:dog_bounds[0]+dog_bounds[2]], (pic_dim, pic_dim))
+            # cv2.imshow("Mask", small)  # Draw just the found object of interest for tuning
+            image_gray = cv2.cvtColor(dog_area, cv2.COLOR_BGR2GRAY)
+            timer = time.time()
+            take_pic_flag = False
+            cv2.imshow("picture", image_gray)
+            time.sleep(0.5)
+            another_pic = cv2.waitKey(2500)
+            if another_pic == ord(" "):
+                take_pic_flag = True
+            cv2.destroyWindow("picture")
+            if take_pic_flag:
+                printv("dog noted")
+                if len(pictures):
+                    pictures = np.append(pictures, [image_gray], axis=0)
+                else:
+                    pictures = np.array([image_gray], dtype=np.uint8)
             else:
-                pictures = np.append(pictures, [image_gray], axis=0)
+                printv("dog rejected")
+        else:
+            printv("no dog detected, no image")
         take_pic_flag = False
-        print(len(pictures))
+        printv(len(pictures))
+    image_det = utils.visualize(image, det_objs)
+
 
     # Add frame rate
     timer2 = time.time()
@@ -103,30 +142,20 @@ while not exit_flag:
                 font_color, font_weight)
     timer = timer2
 
-    # Tensorflow
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    im_tensor = vision.TensorImage.create_from_array(image_rgb)
-    det_objs = detector.detect(im_tensor)
-    image_det = utils.visualize(image, det_objs)
-
-    # Do something with the data
-    for det_obj in det_objs.detections:
-        bounding_box = [det_obj.bounding_box.origin_x,  det_obj.bounding_box.origin_y,
-                        det_obj.bounding_box.width,  det_obj.bounding_box.height]
-        label = det_obj.categories[0].category_name
-
     # Show the image
     cv2.imshow("Camera", image)
     # Exit and saving
     keyHit = cv2.waitKey(1)
+    if keyHit == ord(" "):
+        take_pic_flag = True
     if keyHit == ord("s"):
-        print("Saving")
-        if pictures is None:
-            print("Error, no pictures")
+        printv("Saving")
+        if not len(pictures):
+            printv("Error, no pictures")
         else: 
             f = h5py.File(pictures_file, "a")
             if dset_name not in f.keys():
-                print(pictures.shape)
+                printv(pictures.shape)
                 f.create_dataset(dset_name, data=pictures, chunks=True, 
                                  maxshape=(None,pic_dim,pic_dim))
             else:
