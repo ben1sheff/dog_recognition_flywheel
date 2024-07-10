53f071178f00d286c0a472f338498794e7f8fdd0
ben1sheff <ben1sheff@berkeley.edu>
2024-07-10 14:24:22 -0600

    Added proper model validation to check for overfitting, also the ability to save the model

--- classification_test.py
+++ classification_test.py
@@ -7,9 +7,14 @@ import torch.optim as optim
 import torch.nn as nn
 
 import stance_classifier as sc
-
+# TO DO: this currenty just draws a train vs. validation plot to
+# judge epochs of training needed. This is better done with callback
+# functions, which I'm sure pyTorch supports
 # Parameters
-batch_size = 4
+batch_size = 20
+frac_valid = 0.2
+training_epochs = 15
+model_param_file = "model_parameters"
 
 
 def imshow(img):
@@ -23,6 +28,10 @@ f = h5py.File("pictures.hdf5", "r")
 dset = np.array(f["pics"], dtype="float32")
 tags = torch.from_numpy(np.array(f["sitting"], dtype="float32"))
 dset = list(zip(dset, tags))
+train, valid = torch.utils.data.random_split(dset, [1-frac_valid, frac_valid])
+print("Training on", len(train), "data points, validating on", len(valid), "points")
+# valid = dset[len(dset)*2//3:len(dset)*4//5]
+# dset = dset[:len(dset)*2//3] + dset[len(dset)*4//5:]
 
 # imshow(sc.TestConvolution(3)([dset[4]], 0))
 # imshow(sc.TestConvolution(3)([dset[4]], 1))
@@ -30,41 +39,57 @@ dset = list(zip(dset, tags))
 # plt.show()
 
 classifier = sc.StanceClassifier((512, 512))
-criterion = nn.BCELoss()
-optimizer = optim.Adam(classifier.parameters(), lr=0.001)
-# optimizer = optim.SGD(classifier.parameters(), lr=0.001, momentum=0.9)
+print("Should we load the model? (y/n)")
+if input().startswith("y"):
+    classifier.load_state_dict(torch.load(model_param_file))
+    valid = dset
+else:
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(classifier.parameters(), lr=0.001)
+    # optimizer = optim.SGD(classifier.parameters(), lr=0.001, momentum=0.9)
+    trainloader = torch.utils.data.DataLoader(train, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+    # Prepping validation data
+    val_data = torch.Tensor(np.array([val[0] for val in valid]))
+    val_stance = torch.Tensor(np.array([val[1] for val in valid]))
+    full_data = torch.Tensor(np.array([val[0] for val in train]))
+    full_stance = torch.Tensor(np.array([val[1] for val in train]))
+    losses = []
+    val_losses = []
+    for epoch in range(training_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
 
-trainloader = torch.utils.data.DataLoader(dset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
-for epoch in range(100):
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+            # forward + backward + optimize
+            outputs = classifier(inputs)
+            # print(labels, outputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        # forward + backward + optimize
-        outputs = classifier(inputs)
-        # print(labels, outputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 20 == 19:    # print every 2000 mini-batches
+            # print statistics
+            running_loss += loss.item()
+        if epoch % 10 == 9:    # print every 2000 mini-batches
             print(f'\n[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
+        losses += [criterion(classifier(full_data), full_stance).item()]
+        val_losses += [criterion(classifier(val_data), val_stance).item()]  #  * (1 - frac_valid)/frac_valid]
+    print('Finished Training')
 
-print('Finished Training')
+    plt.plot(list(range(len(losses))), losses, label="raw loss")
+    plt.plot(list(range(len(val_losses))), val_losses, label = "validation")
+    plt.legend()
+    plt.show()
 # print(classifier(torch.tensor([dog[0] for dog in dset])))
-tot = len(dset)
+tot = len(valid)
 correct = 0
 sit_guesses = 0
-for dog, tag in zip(dset, tags):
-    result = classifier(torch.Tensor(dog[0][None,:,:]))
+for dog, tag in valid:
+    result = classifier(torch.Tensor(dog[None,:,:]))
     if (result.item() > 0.5) == (tag.item() > 0.5):
         correct += 1
     if result.item() > 0.5:
@@ -72,3 +97,7 @@ for dog, tag in zip(dset, tags):
     # print(result.item()>0.5, tag.item()>0.5)
 print(correct/tot * 100, "percent correct")
 print("guessed sit", sit_guesses / tot * 100., "percent of the time")
+if len(valid) < len(dset):
+    print("Should we save this? (y/n)")
+    if input().startswith("y"):
+        torch.save(classifier.state_dict(), model_param_file)
--- stance_classifier.py
+++ stance_classifier.py
@@ -8,26 +8,28 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 class StanceClassifier(nn.Module):
     def __init__(self, model_dim: (int, int)):
         super().__init__()
-        conv1_feat = 10
-        conv2_feat = 20
+        conv1_feat = 6
+        conv2_feat = 10
         k_size = 3
         self.pool_size = 3
         self.conv1 = nn.Conv2d(1, conv1_feat, k_size, 1)
         self.conv2 = nn.Conv2d(conv1_feat, conv2_feat, k_size, 1)
-        hid_lay_size = 20 # number of nodes in hidden layer
-        penult_lay_size = 20  # number of nodes in last hidden layer
+        hid_lay_size = 15 # number of nodes in hidden layer
+        penult_lay_size = 15  # number of nodes in last hidden layer
         dim_exp1 = (((model_dim[0] - self.pool_size)//self.pool_size + 1)
                     , ((model_dim[1] - self.pool_size)//self.pool_size + 1))
         dim_exp2 = (((dim_exp1[0] - self.pool_size)//self.pool_size + 1)
                     , ((dim_exp1[1] - self.pool_size)//self.pool_size + 1))
         self.first_lin_layer = nn.Linear(conv2_feat * dim_exp2[0] * dim_exp2[1], hid_lay_size)
+        self.first_lay_norm = nn.LayerNorm(hid_lay_size)
         # self.second_lin_layer = nn.Linear(hid_lay_size, hid_lay_size)
         self.third_lin_layer = nn.Linear(hid_lay_size, penult_lay_size)
         self.classifier = nn.Linear(penult_lay_size, 1)
     def forward(self, data):
         conv1_layer = F.max_pool2d(F.relu(self.conv1(data[:,None,:,:])), self.pool_size)
         conv2_layer = F.max_pool2d(F.relu(self.conv2(conv1_layer)), self.pool_size)
-        first_layer = F.relu(self.first_lin_layer(torch.flatten(conv2_layer, 1)))
+        first_layer = self.first_lay_norm(
+            F.relu(self.first_lin_layer(torch.flatten(conv2_layer, 1))))
         # second_layer = F.relu(self.second_lin_layer(first_layer))
         # Probably should add a dropout layer, maybe one between 1 and 2 as well
         third_layer = F.relu(self.third_lin_layer(first_layer))
